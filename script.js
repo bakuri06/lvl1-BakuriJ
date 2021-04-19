30aeee4699811396042b154b0dfd7204fab45378
unknown <you@example.com>
2021-04-19 18:32:12 +0400

    final version

--- script.js
+++ script.js
@@ -23,35 +23,22 @@ let students = [
   }
 ];
 
-//average
 
-function average(studs,subjs){
-  let average=[];
-  for(let i = 0 ;i<studs.length;i++){
-    for(let j=0;j<subjs.length;j++){
-      if(average[j]==undefined){
-        average[j] = studs[i].grades[j];
-      }else if(average[j]){
-        average[j] += studs[i].grades[j];
-      }
-    }
-  }
-  for(let i=0;i<average.length;i++){
-    average[i]/=subjs.length;
-  }
-
-  return average;
-
-}
-average(students,subjects);
-
-
+let average=[];
 let table = document.getElementById('table');
-
-let thead = document.querySelector('#table thead');
 let tbody = document.querySelector('#table tbody');
+let tfoot =document.querySelector('#table tfoot');
+let del1 = document.querySelector('#delete');
+del1.addEventListener('click',function(){
+  document.getElementById("table").deleteRow(1);
+  students.shift();
+  generateAver();
+})
+
 //generating header
-let tmp = '<tr>'+
+function genHeader(){
+  let thead = document.querySelector('#table thead');
+  let tmp = '<tr>'+
   '<th>Name</th>' + '<th>Last Name</th>';
 
 for(let i=0;i<subjects.length;i++){
@@ -60,73 +47,79 @@ for(let i=0;i<subjects.length;i++){
 
 tmp+='</tr>';
 
-thead.innerHTML += tmp;
-
-//generating average row
-
-tmp='<tr>' + '<td colspan="2" style="text-align:center">Average</td>';
-
-for(let i =0 ;i<average(students,subjects).length;i++){
-  tmp+= `<td class="change">${average(students,subjects)[i]}</td>`;
+thead.innerHTML = tmp;
 }
 
-tmp+='</tr>';
+genHeader();
 
-tbody.innerHTML+=tmp;
+function generateAver(){
+  average = [];
+  for(let i = 0 ;i<students.length;i++){
+    for(let j=0;j<subjects.length;j++){
+      if(average[j]==undefined){
+        average[j] = students[i].grades[j];
+      }else if(average[j]){
+        average[j] += students[i].grades[j];
+      }
+    }
+  }
+
+  for(let i=0;i<average.length;i++){
+    average[i]=Math.floor(average[i]/students.length);
+  }
+  let tmp='<tr>' + '<td colspan="2" style="text-align:center">Average</td>';
+
+  for(let i =0 ;i<average.length;i++){
+     tmp+= `<td>${average[i]}</td>`;
+  }
+
+  tmp+='</tr>';
+
+  tfoot.innerHTML=tmp;
+}
+
+generateAver();
 
 //generating students and grades
 
 tmp='';
 
 for(let i = 0 ;i<students.length;i++){
-  tmp+=`<tr> 
-        <td> ${students[i].name} </td>
-        <td> ${students[i].lastname} </td>`;
-        for(let j=0;j<subjects.length;j++){
-            tmp+=`<td class="${average(students,subjects)[j]<students[i].grades[j] ? 'green' : 'red'}">${students[i].grades[j]} </td>`
-        }
+  addRow(students[i]);
 }
 
 tbody.innerHTML+=tmp;
 
 function Add(){
-  let nameAdd = document.querySelector('#name');
-  let lastAdd = document.querySelector('#lastname');
-  let gradeCss = document.querySelector('#grade1');
-  let gradeHtml = document.querySelector('#grade2');
-  let gradePython = document.querySelector('#grade3');
-  let gradeJs = document.querySelector('#grade4');
-  let add = document.querySelector('#add');
-  let tmp='';
-  students.push(
-    {
-      name: nameAdd.value,
-      lastname: lastAdd.value,
-      grades: [gradeCss.value,gradeHtml.value,gradePython.value,gradeJs.value]
-    }
-  )
+    let nameAdd = document.querySelector('#name');
+    let lastAdd = document.querySelector('#lastname');
+    let gradeCss = document.querySelector('#grade1');
+    let gradeHtml = document.querySelector('#grade2');
+    let gradePython = document.querySelector('#grade3');
+    let gradeJs = document.querySelector('#grade4');
+    students.push(
+      {
+        name: nameAdd.value,
+        lastname: lastAdd.value,
+        grades: [Number(gradeCss.value),Number(gradeHtml.value),Number(gradePython.value),Number(gradeJs.value)]
+      }
+    )
 
-  average(students,subjects);
+    addRow(students[students.length-1]);
+    genHeader();
+    generateAver();
+    
+}
 
-
-  tmp+=`<tr> 
-          <td> ${students[students.length-1].name} </td>
-          <td> ${students[students.length-1].lastname} </td>`;
-
-  //pirobaa gaawitlos tu gaamwvanos
-          
-  for(let i=0;i<subjects.length;i++){
-    tmp+=`<td class="${average(students,subjects)[students.length-1]>students[subjects.length-1].grades[subjects.length-1]?'red':'green'}">${students[students.length-1].grades[i]} </td>`;
-  }
-  
-  tmp+='</tr>';
-  tbody.innerHTML+=tmp;
-  tmp='';
-  let change=document.getElementsByClassName('.change');
-  for(let i =0 ;i<average(students,subjects).length;i++){
-    tmp+= `<td class="change">${average(students,subjects)[i]}</td>`;
-  }
-  change.innerHTML=tmp;
+function addRow(student){
+  let tmp =`<tr> 
+          <td> ${student.name} </td>
+          <td> ${student.lastname} </td>`;
+          for(let i=0;i<student.grades.length;i++){
+              tmp+=`<td class="${average[i]>student.grades[i] ? 'red' : 'green'}">${student.grades[i]} </td>`;
+          }
+  tmp += `</tr>`;
+  document.querySelector('#table tbody').innerHTML+=tmp;
 }
 
 add.addEventListener('click',Add)
