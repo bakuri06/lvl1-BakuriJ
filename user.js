a1ab6f761dcf8a9c87c82a82a52ca3773d524644
unknown <you@example.com>
2021-05-09 11:33:01 +0400

    fibonacci

--- user.js
+++ user.js
@@ -1,37 +1,14 @@
-let input = document.getElementById('uniq');
-
-let show = document.querySelector('.show');
-
-
-let datavalue;
-
-document.querySelector('.but').addEventListener('click',function(){
-    console.log(input.value);
-    fetch(`https://itunes.apple.com/search?term=${input.value}`)
-        .then(response => response.json())
-        .then(data => {
-
-            datavalue = data.results;
-            console.log(datavalue);
-            render();
-        })
-})
-
-function render(){  
-    let smth = '';
-    for(let i=0;i<datavalue.length;i++){
-        smth+=`
-        <div class="container smth">
-            <h2>${datavalue[i].artistName}</h2>
-            <audio controls>
-                <source src="${datavalue[i].previewUrl}" type="audio/mpeg">
-            </audio
-        </div>
-        `;
-
-        console.log(datavalue[i].previewUrl);
-
+function fibonacci(x) {
+    let massive = [1, 1];
+    for (let i = 1; i <= x; i++) {
+        let a = massive[i] + massive[i - 1];
+        if (a < 4000000) {
+            massive.push(a);
+        } else {
+            break;
+        }
     }
-    show.innerHTML+=smth;
-
+    console.log(massive);
+    return massive.filter(i => i % 2 == 0).reduce((a, b) => a + b);
 }
+  
