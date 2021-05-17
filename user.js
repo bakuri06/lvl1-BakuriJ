f6949b4a5cf3ed36a99081e896a2f0c5939c3e9a
bakuri06 <b.jobava1@gmail.com>
2021-05-17 19:30:57 +0400

    changed

--- user.js
+++ user.js
@@ -5,12 +5,20 @@ const prev=document.querySelector('.prev');
 const prev2=document.querySelector('.prev2');
 const sidebar=document.querySelector('.side__bar');
 const sidebar_btn=document.querySelector('.fa.fa-bars.sidebar-bar');
+const cover=document.querySelector('.cover');
 
 if(icon){
     icon.addEventListener('click',function(e){
         burgerMenu.classList.toggle('_active');
     })
+    if(burgerMenu.classList.contains("_active")){
+        console.log('1');
+        cover.addEventListener('click',()=>{
+            burgerMenu.classList.remove("_active");
+        })
+    }
 }
+
 
 prev.addEventListener('click',function(e){
     burgerMenu.classList.remove('_active');
@@ -23,6 +31,8 @@ sidebar_btn.addEventListener("click",function(e){
 prev2.addEventListener('click',function(e){
     sidebar.classList.remove('_active');
 })
+
+
 
 
 
